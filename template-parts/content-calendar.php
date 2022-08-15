e8b236fe2b7e8dee293214e4eb2264f4e09a827a
ErwiseDevelopment <erwisedev@gmail.com>
2022-08-15 12:09:37 -0300

    alteracoes finais

--- template-parts/content-calendar.php
+++ template-parts/content-calendar.php
@@ -53,7 +53,7 @@
                             <div class="col-md-8">
                                 <a 
                                 class="l-news__small__card-read-more u-line-height-100 hover:u-opacity-8 d-block u-font-weight-bold text-center text-decoration-none u-color-folk-white u-bg-folk-secondary py-2 px-5" 
-                                href="<?php ?>">
+                                href="<?php echo get_field('veja_mais_galeria', 'option')?>">
                                     Veja mais
                                 </a>
                             </div>
